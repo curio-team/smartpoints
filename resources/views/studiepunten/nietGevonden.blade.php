78adf26205e4feebbc198b078f6f32fac1039359
Bart Roos <bart@studiokaa.co>
2023-07-05 14:31:58 +0200

    Update nietGevonden.blade.php

--- resources/views/studiepunten/nietGevonden.blade.php
+++ resources/views/studiepunten/nietGevonden.blade.php
@@ -12,7 +12,7 @@
         @endif
 
         <div class=" d-flex justify-content-center align-items-center overflow-hidden" style="height: 100vh">
-            <div class="card m-1 w-50 shadow " style="height: 95%">
+            <div class="card m-1 w-50 shadow " style="height: 60%">
                 <div class="card-body overflow-auto p-3 text-center">
                     <h1>Studiepunten niet gevonden.</h1>
                 </div>
