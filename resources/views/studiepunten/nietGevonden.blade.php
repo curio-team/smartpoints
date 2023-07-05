59718304119ad1c5403096d26d800a227acc53e7
Bart Roos <bart@studiokaa.co>
2023-07-05 14:30:47 +0200

    Update nietGevonden.blade.php

--- resources/views/studiepunten/nietGevonden.blade.php
+++ resources/views/studiepunten/nietGevonden.blade.php
@@ -1,6 +1,16 @@
 <x-layout>
     <x-layout>
-        {{-- @dd($studiepunten) --}}
+        
+        @if(Auth::user()->type == "teacher")
+            <div class="position-absolute w-100 shadow bg-body-secondary p-3">
+                <form action="{{ route('student') }}" method="POST">
+                    @csrf
+                    <input type="text" name="nummer" placeholder="123456" value="{{ $nummer ?? '' }}">
+                    <input type="submit" value="Opzoeken" />
+                </form>
+            </div>
+        @endif
+
         <div class=" d-flex justify-content-center align-items-center overflow-hidden" style="height: 100vh">
             <div class="card m-1 w-50 shadow " style="height: 95%">
                 <div class="card-body overflow-auto p-3 text-center">
